3d99f3c99a91006c66c69d2db5a37cb96eecf85d
Arjunverma07 <142525442+Arjunverma07@users.noreply.github.com>
2024-05-14 14:26:48 +0530

    Update 002. Largest Local Values in a Matrix.cpp

--- leet-code-daily-problem/002. Largest Local Values in a Matrix.cpp
+++ leet-code-daily-problem/002. Largest Local Values in a Matrix.cpp
@@ -3,7 +3,7 @@ class Solution {
   vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
     const int n = grid.size();
     vector<vector<int>> ans(n - 2, vector<int>(n - 2));
-
+   
     for (int i = 0; i < n - 2; ++i)
       for (int j = 0; j < n - 2; ++j)
         for (int x = i; x < i + 3; ++x)
